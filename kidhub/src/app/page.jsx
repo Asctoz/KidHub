61c275c0914d123af82aed17b4854492b75eaa38
Asctoz <oanother065@gmail.com>
2025-07-16 20:34:48 -0700

    jkhkhkjh

--- kidhub/src/app/page.jsx
+++ kidhub/src/app/page.jsx
@@ -1,15 +1,16 @@
 "use client";
 
 import HighlightPage from "@/components/HighlightPage";
-import Navbar from "@/components/Navbar.jsx"
+import Navbar from "@/components/Navbar.jsx";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <HighlightPage className="absolute" />
-      <Navbar className="absolute top-1" />
-
+    <div className="relative min-h-screen">
+      {/* Navbar overlays the content */}
+      <Navbar className="fixed top-0 left-0 w-full z-50" />
+      
+      {/* Hero content sits underneath */}
+      <HighlightPage className="pt-[64px] relative" /> {/* Adjust pt-[64px] to match navbar height */}
     </div>
-
   );
 }
