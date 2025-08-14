f2b06c9d6da9b362936c82e8dfb01a7626705e5c
Asctoz <oanother065@gmail.com>
2025-08-14 09:03:24 +0530

    asdfghjkloiuy

--- kidhub/src/components/Navbar.jsx
+++ kidhub/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function FloatingDockDemo() {
       icon: (
         <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: "#",
+      href: "/",
     },
     {
       title: "Websites",
